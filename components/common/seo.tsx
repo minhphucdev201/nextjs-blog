c82e5eb4a5b2b0318d49f9c679d0ae9affc89dbe
Minh Phuc <minhphuc201.dev@gmail.com>
2023-05-16 20:57:25 +0700

    fix meta tags

--- components/common/seo.tsx
+++ components/common/seo.tsx
@@ -22,22 +22,14 @@ export function Seo({ data }: SeoProps) {
 
       <meta property="og:type" content="website" />
       <meta property="og:url" content={url} />
-      <meta property="og:title" content="Minh Phúc Blog: Trang chủ" />
-      <meta
-        property="og:description"
-        content="Nơi kiến thức Frontend được chia sẻ một cách đơn giản, dễ hiểu và đặc biệt là vui.
-Bạn đã truy cập trang này 2 lần"
-      />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content={thumbnailUrl} />
 
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={url} />
-      <meta property="twitter:title" content="Minh Phúc Blog: Trang chủ" />
-      <meta
-        property="twitter:description"
-        content="Nơi kiến thức Frontend được chia sẻ một cách đơn giản, dễ hiểu và đặc biệt là vui.
-Bạn đã truy cập trang này 2 lần"
-      />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={description} />
       <meta property="twitter:image" content={thumbnailUrl} />
     </Head>
   )
